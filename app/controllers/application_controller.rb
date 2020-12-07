d6e21c9998ce2d2e75fcead03177081f4cad6776
Alicia Weenum <weenum.alicia@gmail.com>
2020-12-07 13:46:02 -0500

    commit tree f600185e223455e6a4e4f5b9429af08bd3c161dd

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,4 +7,17 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
+  get '/new' do
+    erb :new
+  end
+
+  # get /homepage do
+  #   erb :new
+  # end
+
 end
+
+#Once a user is logged in, they should be able to create, 
+#edit and delete their own tweets, as well as view all the tweets.
+
+
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,3 +2,10 @@ class User < ActiveRecord::Base
   has_secure_password
   has_many :tweets
 end
+
+
+#@username = username
+#@email = email
+#@password = password
+
+
